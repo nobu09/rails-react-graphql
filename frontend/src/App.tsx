83b3b2dd624b139ddd1dd973064ec87eebe9cf11
nobu09 <11857481+nobu09@users.noreply.github.com>
2023-09-20 14:42:31 +0900

    Mutationを使う

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
-import { useBooksQuery } from './graphql/generated';
+import { useBooksQuery, useCreateBookMutation } from './graphql/generated';
 
 function App() {
   const { data: { books = [] } = {} } = useBooksQuery();
+  const [createBook] = useCreateBookMutation({refetchQueries: ['books']})
+  const [title, setTitle] = useState('');
 
   return (
     <div>
-      {books.map((book) => (
-        <div key={book.id}>{book.title}</div>
-      ))}
+      <input value={title} onChange={(e) => setTitle(e.target.value)} />
+
+      <button
+        onClick={() => {
+          createBook({ variables: { params: { title: title }}});
+          setTitle('');
+        }}>
+        保存
+      </button>
+
+      <div>
+        {books.map((book) => (
+          <div key={book.id}>{book.title}</div>
+        ))}
+      </div>
     </div>
   );
 }
