406b9f65db961162ba86d96c911ecbb7a0982b4b
nobu09 <11857481+nobu09@users.noreply.github.com>
2023-09-21 11:05:13 +0900

    useDeleteBookMutationを呼ぶようにした

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,12 @@
 import React, { useState } from 'react';
 import './App.css';
-import { useBooksQuery, useCreateBookMutation } from './graphql/generated';
+import { useBooksQuery, useCreateBookMutation, useDeleteBookMutation } from './graphql/generated';
 
 function App() {
   const { data: { books = [] } = {} } = useBooksQuery();
   const [createBook] = useCreateBookMutation({refetchQueries: ['books']})
   const [title, setTitle] = useState('');
+  const [deleteBook] = useDeleteBookMutation({refetchQueries: ['books']})
 
   return (
     <div>
@@ -21,7 +22,12 @@ function App() {
 
       <div>
         {books.map((book) => (
-          <div key={book.id}>{book.title}</div>
+          <div key={book.id}>
+            <div>{book.title}</div>
+            <button onClick={() => deleteBook({ variables: { id : book.id }})}>
+              削除
+            </button>
+          </div>
         ))}
       </div>
     </div>
