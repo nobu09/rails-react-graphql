fb78a487748f98f73e75a9e28b9ff8ae59a2a098
nobu09 <11857481+nobu09@users.noreply.github.com>
2023-09-27 08:42:23 +0900

    スタイル調整

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -10,20 +10,23 @@ function App() {
   const [updateBook] = useUpdateBookMutation({refetchQueries: ['books']})
 
   return (
-    <div>
-      <input value={title} onChange={(e) => setTitle(e.target.value)} />
+    <div style={{ width: "400px", margin: "40px auto"}}>
+      <h1>書籍一覧</h1>
+      <div style={{ display: "flex", gap: "10px", marginBottom: "40px" }}>
+        <input value={title} onChange={(e) => setTitle(e.target.value)} />
 
-      <button
-        onClick={() => {
-          createBook({ variables: { params: { title: title }}});
-          setTitle('');
-        }}>
-        保存
-      </button>
+        <button
+          onClick={() => {
+            createBook({ variables: { params: { title: title }}});
+            setTitle('');
+          }}>
+          保存
+        </button>
+      </div>
 
       <div>
         {books.map((book) => (
-          <div key={book.id}>
+          <div key={book.id} style={{ display: "flex", gap: "10px", marginBottom: "10px" }}>
             <div>{book.title}</div>
             <input value={book.title || ""} onChange={(e) => updateBook({ variables: { id: book.id, params: { title: e.target.value } } })} />
             <button onClick={() => deleteBook({ variables: { id : book.id }})}>
